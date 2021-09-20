8d1aa3d7f09dd502d0a3f18a5702165fdddb1b5a
SathvikGurupalli <sathvikgcol@gmail.com>
2021-09-20 12:17:59 -0500

    Commit for TA

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgview;
-    //hello
+    //hello world
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
